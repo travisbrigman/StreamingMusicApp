d28bcc016590e54ff3a7a316bed5a7b2de14e310
travisbrigman <travis1000@gmail.com>
2021-08-22 06:19:26 -0500

    Initial Commit

---
+++ StreamingMusicApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StreamingMusicApp
+//
+//  Created by Travis Brigman on 8/22/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StreamingMusicApp/StreamingMusicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreamingMusicAppApp.swift
+//  StreamingMusicApp
+//
+//  Created by Travis Brigman on 8/22/21.
+//
+
+import SwiftUI
+
+@main
+struct StreamingMusicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
